9f8eef980c7786eb21907f80eca7778de259844e
alauhy <751961605@qq.com>
2019-07-12 14:56:17 +0800

    finished add

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,23 +2,46 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int sum = 0;
+        if (leftBorder > rightBorder) {
+            int tmp = leftBorder;
+            leftBorder = rightBorder;
+            rightBorder = tmp;
+        }
+        for (int i = leftBorder; i <= rightBorder; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+
+        }
+        return sum;
     }
+
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
         throw new NotImplementedException();
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+
+        return arrayList.stream().map((e) -> e * 3 + 2).reduce(0, (x, y) -> x + y);
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> newArrayList = new ArrayList<Integer>();
+        for (Integer i : arrayList) {
+            if (i % 2 == 0) {
+                newArrayList.add(i);
+            }
+            else newArrayList.add(i*3+2);
+        }
+        return newArrayList;
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
@@ -30,7 +53,9 @@ public class Add {
 //    }
 
     public double getAverageOfEven(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> list=new ArrayList<Integer>();
+        list=arrayList.stream().filter((e)->e%2==0).collect(Collectors.toList());
+        return list.stream().reduce(0,(x,y)->x+y)/list.size();
     }
 
     public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
